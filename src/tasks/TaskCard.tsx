a43f8fa38a713d35a6c4e1d290de58a61048c898
Stephen <stephensouness@outlook.com>
2026-04-03 13:29:09 +0100

    fix(TaskCard): move Link to outer container to restore behaviour

--- src/tasks/TaskCard.tsx
+++ src/tasks/TaskCard.tsx
@@ -13,20 +13,20 @@ export default function TaskCard({ task }) {
     : undefined;
 
   return (
-    <div
-      ref={setNodeRef}
-      {...listeners}
-      {...attributes}
-      className="mb-3 block rounded bg-gray-100 p-3 shadow-md"
-      style={style}
-    >
-      {transform ? (
-        <h3 className="font-semibold wrap-break-word">{task.title}</h3>
-      ) : (
-        <Link to={`/tasks/${task.id}`}>
+    <Link to={`/tasks/${task.id}`}>
+      <div
+        ref={setNodeRef}
+        {...listeners}
+        {...attributes}
+        className="mb-3 block rounded bg-gray-100 p-3 shadow-md"
+        style={style}
+      >
+        {transform ? (
           <h3 className="font-semibold wrap-break-word">{task.title}</h3>
-        </Link>
-      )}
-    </div>
+        ) : (
+          <h3 className="font-semibold wrap-break-word">{task.title}</h3>
+        )}
+      </div>
+    </Link>
   );
 }
